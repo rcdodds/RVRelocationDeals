684e8162c15a860e8f81a5c7340b2ec8e1387708
Ryan Dodds <rcdodds@gmail.com>
2020-09-08 14:16:12 -0400

    Remove PAUSE so CMD closes

---
+++ ScrapeRVs.bat
@@ -0,0 +1,5 @@
+:: Turn off command logging
+ECHO off
+
+:: Run python script to get RV Relocation Deals
+"C:\Users\rcdodds\Documents\GitHub\RVRelocationDeals\venv\Scripts\python.exe" "C:\Users\rcdodds\Documents\GitHub\RVRelocationDeals\RelocationScraping.py"
